27e7637ff61bf3aed940b0fcb621eb99165107d5
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-05-06 08:58:11 -0600

    created router and rout handling

--- routes/articles.js
+++ routes/articles.js
@@ -1,19 +1,33 @@
 const express = require("express");
-
+const Article = require("./../models/article");
 const router = express.Router();
 
 // Display form to create a new article
 router.get("/new", (req, res) => {
-  res.render("articles/new");
+  res.render("articles/new", { article: new Article() });
+});
+
+router.get("/:id", async (req, res) => {
+  const article = await Article.findById(req.params.id);
+
+  if (article == null) return res.redirect("/");
+  res.render("articles/show", { article: article });
 });
 
 // Handle form submission to create a new article
-router.post("/", (req, res) => {
-  // Here you can access the form data from req.body
-
-  // Example: Save the article to the database or perform other actions
-  // For now, let's just send a response
-  res.send("Article created successfully!");
+router.post("/", async (req, res) => {
+  let article = new Article({
+    title: req.body.title,
+    description: req.body.description,
+    markdown: req.body.markdown,
+  });
+  try {
+    article = await article.save();
+    res.redirect(`/articles/${article.id}`);
+  } catch (e) {
+    console.log(e);
+    res.render("articles/new", { article: article });
+  }
 });
 
 module.exports = router;
