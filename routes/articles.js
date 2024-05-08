e6ef2a94fd0a4e34d0c8651f81892be5fcbb90ed
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-05-08 17:52:04 -0600

    created an edit file and make sure it works

--- routes/articles.js
+++ routes/articles.js
@@ -7,6 +7,11 @@ router.get("/new", (req, res) => {
   res.render("articles/new", { article: new Article() });
 });
 
+router.get("/edit/:id", async (req, res) => {
+  const article = await Article.findById(req.params.id);
+  res.render("articles/edit", { article: article });
+});
+
 router.get("/:id", async (req, res) => {
   const article = await Article.findById(req.params.id);
 
@@ -15,20 +20,24 @@ router.get("/:id", async (req, res) => {
 });
 
 // Handle form submission to create a new article
-router.post("/", async (req, res) => {
-  let article = new Article({
-    title: req.body.title,
-    description: req.body.description,
-    markdown: req.body.markdown,
-  });
-  try {
-    article = await article.save();
-    res.redirect(`/articles/${article.id}`);
-  } catch (e) {
-    console.log(e);
-    res.render("articles/new", { article: article });
-  }
-});
+router.post(
+  "/",
+  async (req, res, next) => {
+    req.article = new Article();
+    next();
+  },
+  saveArticleAndRedirect("new")
+);
+router.put(
+  "/:id",
+  async (req, res, next) => {
+    req.article = await Article.findById(req.params.id);
+    next();
+  },
+  saveArticleAndRedirect("new")
+);
+
+router.put("/:id", (req, res) => {});
 
 router.delete("/:id", async (req, res) => {
   try {
@@ -39,5 +48,21 @@ router.delete("/:id", async (req, res) => {
     res.status(500).send("Error deleting article");
   }
 });
+
+function saveArticleAndRedirect(path) {
+  return async (req, res) => {
+    let article = req.article;
+    article.title = req.body.title;
+    article.description = req.body.description;
+    article.markdown = req.body.markdown;
+    try {
+      article = await article.save();
+      res.redirect(`/articles/${article.id}`);
+    } catch (e) {
+      console.log(e);
+      res.render(`articles/${path}`, { article: article });
+    }
+  };
+}
 
 module.exports = router;
